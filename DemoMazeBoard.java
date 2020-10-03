46e568b3b9cef1aae48021a345cccc64eb09dfd5
synfy <alexandra.hinton18@ncf.edu>
2020-10-03 17:04:27 -0400

    added comment

--- DemoMazeBoard.java
+++ DemoMazeBoard.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 /**
- * 
+ *
  * @author fahmidahamid
  *
  */
@@ -13,24 +13,24 @@ import java.util.Scanner;
  * https://rosettacode.org/wiki/Maze_generation#Java
  * I have taken some idea for implementing the printMazeBoard(...)
  */
-
+//
 class MazeBoard
 {
 	private int d;
 	private Cell[][] board;
-	
+
 	public MazeBoard(int d){
 		this.d = d;
 		board = new Cell[d][d];
 
 		/*
-		 * Initially all the cells have walls all around them 
+		 * Initially all the cells have walls all around them
 		 */
 		for(int i = 0; i < d; i++)
 			for(int j = 0; j < d; j++)
 				board[i][j] = new Cell(i,j);
 	}
-	
+
 	public boolean updateCell(int i, int j, char objName)
 	{
 		if((i < this.d && i >= 0) && (j<this.d && j >= 0)) {
@@ -40,7 +40,7 @@ class MazeBoard
 		else
 			return false;
 	}
-	
+
 	public void printMazeBoard()
 	{
 		for(int i = 0; i < this.d; i++) {
@@ -62,31 +62,31 @@ class MazeBoard
 		for (int j = 0; j < this.d; j++) {
 			System.out.print("+---");
 		}
-		System.out.println("+");	
+		System.out.println("+");
 	}
-	
-	
+
+
 	public void primsAlgorithm(int rootx, int rooty)
 	{
-	
-	
-		
-		
+
+
+
+
 	}
-		
+
 }
 
 
 public class DemoMazeBoard {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scr = new Scanner(System.in);
 		int d = scr.nextInt();
 		if(d >= 4 && d <= 10)
 		{
-		 MazeBoard mzb = new MazeBoard(d);	
-		
+		 MazeBoard mzb = new MazeBoard(d);
+
 		 mzb.updateCell(0, 3, 'S');
 		 mzb.updateCell(2, 2, 'D');
 		 mzb.updateCell(d-1, d-1, 'A');
@@ -94,10 +94,7 @@ public class DemoMazeBoard {
 		 mzb.printMazeBoard();
 		 mzb.primsAlgorithm(0, 0);
 		}
-		
-		
+
+
 	}
 }
-	
-	
-
