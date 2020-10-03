593b414451230cf93627aa3d087ee61d5079b249
synfy <alexandra.hinton18@ncf.edu>
2020-10-03 17:01:50 -0400

    Initial

---
+++ Cell.java
@@ -0,0 +1,89 @@
+class Wall {
+	
+	protected boolean up, down, left, right;
+	
+	Wall()
+	{
+		//by default, we add the walls
+		up = down = left = right = true;
+	}
+	
+	public void removeWall(char c)
+	{
+		if(c == 'l') left = false;
+		else if (c == 'r') right = false;
+		else if (c == 'u') up = false;
+		else if (c == 'd') down = false;
+		else
+			return;
+	}
+	public void addWall(char c)
+	{
+		if(c == 'l') left = true;
+		else if (c == 'r') right = true;
+		else if (c == 'u') up = true;
+		else if (c == 'd') down = true;
+		else
+			return;
+	}
+	
+}
+
+
+public class Cell {
+	
+	protected int i;
+	protected int j; 
+	protected boolean emptyCell; // true -> cell is empty, false -> cell is occupied 
+	protected char objName;
+	protected Wall cWall;
+	protected boolean visited;
+	protected int priority;
+	
+	public Cell(int i, int j){
+		this.i = i;
+		this.j = j;
+		this.emptyCell = true; // by default we assume that the cell is empty
+		this.objName = ' '; // since the cell is empty, no one is occupying it	
+		cWall = new Wall();
+		this.visited = false;
+		this.priority = 1;
+	}
+	
+	public boolean putObject(char objName)
+	{
+		if (objName != 'P' && objName != 'D' && objName != 'S' && objName != 'B' && objName != 'A') {
+			System.out.println("You are trying to put invalid/unknown object in a cell");
+			return false; //operation unsuccessful
+		}
+		else {
+			this.objName = objName;
+			this.emptyCell = false;
+			return true; //operation successful
+		}
+	}
+	
+	
+	public void replaceObject()
+	{
+		if(!isEmpty())
+		{
+			this.emptyCell = true;
+			this.objName = ' ';
+		}
+	}
+	
+	public char whoIsHere() {
+		return this.objName;
+	}
+	
+	public boolean isEmpty()
+	{
+		return emptyCell;
+	}
+	
+	
+	
+	
+
+}
---
+++ DemoMazeBoard.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+/**
+ * 
+ * @author fahmidahamid
+ *
+ */
+
+
+/* Some good resources
+ * https://rosettacode.org/wiki/Maze_generation#Java
+ * I have taken some idea for implementing the printMazeBoard(...)
+ */
+
+class MazeBoard
+{
+	private int d;
+	private Cell[][] board;
+	
+	public MazeBoard(int d){
+		this.d = d;
+		board = new Cell[d][d];
+
+		/*
+		 * Initially all the cells have walls all around them 
+		 */
+		for(int i = 0; i < d; i++)
+			for(int j = 0; j < d; j++)
+				board[i][j] = new Cell(i,j);
+	}
+	
+	public boolean updateCell(int i, int j, char objName)
+	{
+		if((i < this.d && i >= 0) && (j<this.d && j >= 0)) {
+			board[i][j].putObject(objName);
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public void printMazeBoard()
+	{
+		for(int i = 0; i < this.d; i++) {
+				// draw the north edge
+				for (int j = 0; j < this.d; j++) {
+					System.out.print("+---");
+				}
+				System.out.println("+");
+				// draw the west edge
+				for (int j = 0; j < this.d; j++) {
+					//if(this.board[i][j].isEmpty())
+					 System.out.print("| " + board[i][j].whoIsHere() + " ");
+					//else
+					// System.out.print("|   ");
+				}
+				System.out.println("|");
+		 }
+		// draw the bottom line
+		for (int j = 0; j < this.d; j++) {
+			System.out.print("+---");
+		}
+		System.out.println("+");	
+	}
+	
+	
+	public void primsAlgorithm(int rootx, int rooty)
+	{
+	
+	
+		
+		
+	}
+		
+}
+
+
+public class DemoMazeBoard {
+
+	public static void main(String[] args) {
+		
+		Scanner scr = new Scanner(System.in);
+		int d = scr.nextInt();
+		if(d >= 4 && d <= 10)
+		{
+		 MazeBoard mzb = new MazeBoard(d);	
+		
+		 mzb.updateCell(0, 3, 'S');
+		 mzb.updateCell(2, 2, 'D');
+		 mzb.updateCell(d-1, d-1, 'A');
+		 mzb.updateCell(0, 0, 'B');
+		 mzb.printMazeBoard();
+		 mzb.primsAlgorithm(0, 0);
+		}
+		
+		
+	}
+}
+	
+	
+
